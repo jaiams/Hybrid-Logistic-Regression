63b506879d6947d0b57bfe87776965e3ed4e36ed
Jaiam James Sarabillo <50316148+jaiams@users.noreply.github.com>
2020-11-15 13:29:20 +0800

    Updated Baseline Code
    Remove useless Variables and libraries

--- BASELINE.py
+++ BASELINE.py
@@ -1,35 +1,24 @@
 import sys, csv
-from textblob import TextBlob 
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-
+#----------LOGISITC REGRESSION PART------------
 from sklearn.model_selection import train_test_split
-
 from sklearn.preprocessing import StandardScaler 
 from sklearn.metrics import accuracy_score
+from numpy import log, dot, e
+from numpy.random import rand
 from sklearn.metrics import confusion_matrix, classification_report   
 
 from sklearn.datasets import make_classification
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.over_sampling import SMOTE
-
-
 from collections import Counter
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from threading import Thread
-from functools import wraps
 
 
-
-from numpy import log, dot, e, where
-from numpy.random import rand
 
 
 
@@ -37,14 +26,24 @@ from numpy.random import rand
 class LogisticRegression():
     
     def sigmoid(self, z): return 1 / (1 + e**(-z))
+
     
+    def softmax(self,x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0)
+  
+    def tanh(self,z):
+        ez = np.exp(z)
+        enz = np.exp(-z)
+        return (ez - enz)/ (ez + enz)
+
     def cost_function(self, X, y, weights):                 
         z = dot(X, weights)
         predict_1 = y * log(self.sigmoid(z))
         predict_0 = (1 - y) * log(1 - self.sigmoid(z))
-        return -sum(predict_1 + predict_0) / len(X)
+        return -sum(predict_1 + predict_0 ) / len(X)
     
-   
+
     
     def fit(self, X, y, epochs=25, lr=0.05):        
         loss = []
@@ -54,7 +53,10 @@ class LogisticRegression():
         for _ in range(epochs):        
             # Gradient Descent
             y_hat = self.sigmoid(dot(X, weights))
-            weights -= lr * dot(X.T,  y_hat - y) / N            
+            x_hat = self.softmax(dot(X, weights))
+            z_hat = self.tanh(dot(X, weights))
+            
+            weights -= lr * dot(X.T,  y_hat - x_hat - z_hat- y) / N            
             # Saving Progress
             loss.append(self.cost_function(X, y, weights)) 
             
@@ -63,45 +65,64 @@ class LogisticRegression():
     
     
     def predict(self, X):        
-        # Predicting with sigmoid function
+        # Predicting with sigmoid,Softmax and tanh function
         z = dot(X, self.weights)
+     
+        soft = self.softmax(z)
+        
+        tand = self.tanh(soft)
+        
+    
         # Returning binary result
         x = []
-        print (z)
-        for i in z:
-            if i > 0.0:
+        for i in tand:
+            if i > 0.05:
                 x.append(1)
                 print("{:.2f}".format(i),': 1')
-            else:
+            elif (i >= .01) and (i <=0.059):
                 x.append(0)
-                print("{:.2f}".format(i),': 0')
+                print("{:.2f}".format(i),": 0")
+            else:
+                x.append(-1)
+                print("{:.2f}".format(i),': -1')
                 
         return x
 
-class baseline_algo():
+
+
+
+class logistic_regression():
     
     
     
     
-    def BASELINE_METHOD(self):
+    def Preprocess(self):
+        
         #-----SPLIT DATASETS-------
         tested = pd.read_csv(r'test.csv')
         x = tested.iloc[:, [5, 6]].values   
         # output 
         y = tested.iloc[:, 7].values
-        xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.70, random_state = 42) 
-      
+        xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.40, random_state = 42) 
+
         #4 Feature Scaling
+
+
+        #Feature Scaling or Standardization: It is a step of Data Pre Processing which is applied to independent variables or features of data. 
+        # It basically helps to normalise the data within a particular range. Sometimes, it also helps in speeding up the calculations in an algorithm.
+
+
         sc_x = StandardScaler() 
         xtrain = sc_x.fit_transform(np.asarray(xtrain))  
-        xtest = sc_x.transform(np.asarray(xtest)) 
-        
-        
-        counter = Counter(y)
-        #---------------SMOTE ALGORITHM--------------------------
+        xtest = sc_x.transform(np.asarray(xtest))
+
+        counter = Counter(y) 
+
+
+      #---------------SMOTE ALGORITHM--------------------------
     
         print("Before OverSampling, counts of label '1': {}\n".format(sum(ytrain == 1))) 
-        print("Before OverSampling, counts of label '-1': {} \n".format(sum(ytrain == 0))) 
+        print("Before OverSampling, counts of label '-1': {} \n".format(sum(ytrain == -1))) 
         print('WITH SMOTE')
 
         os =  RandomOverSampler(sampling_strategy='minority')
@@ -116,13 +137,29 @@ class baseline_algo():
         print('After OverSampling, the shape of train_y: {} \n'.format(ytrain.shape)) 
     
         print("After OverSampling, counts of label '1': {}".format(sum(ytrain == 1))) 
-        print("After OverSampling, counts of label '-1': {}".format(sum(ytrain == 0)))
-        #---------------LOGISTIC REGRESSION----------------------
+        print("After OverSampling, counts of label '-1': {}".format(sum(ytrain == -1)))
+        
 
-        #sklearn.linear_model LogisticRegression Interpretation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #---------------LOGISTIC REGRESSION----------------------
         #5 Fitting the Logistic Regression to the Training Set: 
-    
-        #There are many optional parameters. Lets only use random_state=0
         #We create a classifier object of LR class
         
         classifier = LogisticRegression()
@@ -130,27 +167,30 @@ class baseline_algo():
 
         #Fit logistic regression model to the training set (Xtrain and ytrain)
         classifier.fit(xtrain, ytrain)
-        
        
-       
+    
         #6 Predicting the Test set results
         #Using predict method for the classifier object and put Xtest for #argument
         y_pred = classifier.predict(xtest)
         print(y_pred)
-        posed = 0
-        neued = 0
-        neged = 0
+        posed = 1
+        neued = 1
+        neged = 1
+
+        
+
+
         #-----------The Result On The Logistic Regression Process Based on the Number of Test size will be seperated and determine the overall Result--------------
         for over in y_pred:
           
             if over == 1:
                 posed+=1
-              
+            elif over ==0:
+                neued +=1     
             else: 
                 neged+=1
-
-        counter = Counter(y)
         
+    
 
         #---------------CONFUSION MATRIX----------------------
         #7 Making the Confusion Matrix. It contains the correct and incorrect predictions of our model 
@@ -164,21 +204,27 @@ class baseline_algo():
         
         print ("Confusion Matrix : \n", cm)   
         print (cr)
+
         import mlxtend.plotting
         from mlxtend.plotting import plot_confusion_matrix
-        class_names = ['-1','0', '1']
-        fig, ax = plot_confusion_matrix(conf_mat=cm,colorbar=True,
-                                class_names=class_names)
-        fig.canvas.set_window_title('BASELINE LOGISTIC REGRESSION')
+        class_names = ['-1', '0', '1']
+        fig, ax = plot_confusion_matrix(conf_mat=cm,colorbar=True,class_names=class_names)
+        fig.canvas.set_window_title('HYBRID LOGISTIC REGRESSION')
         plt.ylabel('Actual label')
         plt.xlabel('Predicted label')
         plt.show()
 
 
-
-
         
-        #-----SENDS ALL VALUES TO APPEAR ON THE UI----------------
+        
+
+
+
+
+
+
+
+        #-----SENDS ALL VALUES TO APPEAR ON THE USER INTERFACE----------------
         global accurate, confuse,posi, neut, nega, overall,plots,replot,percentage, reports
         accurate = accuracy_score(ytest, y_pred)
         print(accurate)
@@ -192,9 +238,7 @@ class baseline_algo():
         replot = plt
         reports = cr
 
-        pos = 1
-        neu = 1
-        neg = 1
+       
 
 
         if (neut >= posi) and (neut >= nega):
@@ -213,7 +257,7 @@ class baseline_algo():
 
     
                 
-object = baseline_algo()
-object.BASELINE_METHOD()
+object = logistic_regression()
+object.Preprocess()
 
 
